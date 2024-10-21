9e7960ae1b253a8fcdff03f661f9bbbcf26ca2d2
Gyubin-Han <yangglei2@naver.com>
2024-10-21 11:09:02 +0900

    first commit

---
+++ src/main/java/shop/S5G/shop/ShopApplication.java
@@ -0,0 +1,13 @@
+package shop.S5G.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopApplication.class, args);
+	}
+
+}
