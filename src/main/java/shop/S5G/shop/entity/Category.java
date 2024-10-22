80137f03e3335c488efe6d0dbafe1edb84c8a511
Jung-won-seok <ukksj0621@naver.com>
2024-10-22 13:34:20 +0900

    feat : 도서crud
    body : 도서crud
    
    footer :

--- src/main/java/shop/S5G/shop/entity/Category.java
+++ src/main/java/shop/S5G/shop/entity/Category.java
@@ -1,4 +1,22 @@
 package shop.S5G.shop.entity;
 
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int category_id;
+
+    @ManyToOne
+    @JoinColumn(name = "category_super_id")
+    private Category parent_category;
+
+    @OneToMany(mappedBy = "parent_category")
+    private List<Category> children_category;
+
+    private String category_name;
+    private boolean active;
 }
