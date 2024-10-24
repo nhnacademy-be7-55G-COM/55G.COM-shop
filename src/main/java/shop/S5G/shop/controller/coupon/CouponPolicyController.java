7595ae9a8094cdeea04fbee4e18b2ced3ebecee4
DooYoungHo <pimang223@naver.com>
2024-10-24 22:13:52 +0900

    Feat: 쿠폰 정책 관련 CRUD 작성 couponPolicy - exception 작성 couponPolicy - entity, dto, repo, service 구현

---
+++ src/main/java/shop/S5G/shop/controller/coupon/CouponPolicyController.java
@@ -0,0 +1,13 @@
+package shop.S5G.shop.controller.coupon;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+import shop.S5G.shop.service.coupon.CouponPolicyService;
+
+@RestController
+@RequiredArgsConstructor
+public class CouponPolicyController {
+
+    //TODO - Doo :  쿠폰 정책 컨트롤러 마무리 하기
+    private final CouponPolicyService couponPolicyService;
+}
