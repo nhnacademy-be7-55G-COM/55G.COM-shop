3e0976b9f20322dcbcbff3d5dea9cff8988cec13
FlowerfulFort <skagyghk@naver.com>
2024-10-25 09:55:14 +0900

    Fix: Repository Generic modified #26 레포지터리 타입 제네릭 수정

--- src/main/java/shop/S5G/shop/repository/order/DeliveryFeeRepository.java
+++ src/main/java/shop/S5G/shop/repository/order/DeliveryFeeRepository.java
@@ -1,7 +1,8 @@
 package shop.S5G.shop.repository.order;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import shop.S5G.shop.entity.order.DeliveryFee;
 
-public interface DeliveryFeeRepository extends JpaRepository<DeliveryFeeRepository, Long> {
+public interface DeliveryFeeRepository extends JpaRepository<DeliveryFee, Long> {
 
 }
--- src/main/java/shop/S5G/shop/repository/order/OrderDetailTypeRepository.java
+++ src/main/java/shop/S5G/shop/repository/order/OrderDetailTypeRepository.java
@@ -1,7 +1,8 @@
 package shop.S5G.shop.repository.order;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import shop.S5G.shop.entity.order.OrderDetailType;
 
-public interface OrderDetailTypeRepository extends JpaRepository<OrderDetailTypeRepository, Long> {
+public interface OrderDetailTypeRepository extends JpaRepository<OrderDetailType, Long> {
 
 }
