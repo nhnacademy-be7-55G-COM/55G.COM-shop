b1f7b25c7e9db7070eab60957217cc7232e80174
DooYoungHo <pimang223@naver.com>
2024-10-25 09:08:15 +0900

    Feat : 테스트 코드 주석 처리

--- src/main/java/shop/S5G/shop/controller/coupon/CouponPolicyController.java
+++ src/main/java/shop/S5G/shop/controller/coupon/CouponPolicyController.java
@@ -1,6 +1,7 @@
 package shop.S5G.shop.controller.coupon;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import shop.S5G.shop.service.coupon.CouponPolicyService;
 
@@ -10,4 +11,5 @@ public class CouponPolicyController {
 
     //TODO - Doo :  쿠폰 정책 컨트롤러 마무리 하기
     private final CouponPolicyService couponPolicyService;
+
 }
