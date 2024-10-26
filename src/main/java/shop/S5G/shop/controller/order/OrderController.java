b88f98dbdf68b45d8d167d0e37c675e2944be083
FlowerfulFort <skagyghk@naver.com>
2024-10-26 17:52:39 +0900

    Fix/Test: Output changed to PageResponseDto #26 List -> PageResponseDto, 데이터 있는 경우 테스트케이스 추가

--- src/main/java/shop/S5G/shop/controller/order/OrderController.java
+++ src/main/java/shop/S5G/shop/controller/order/OrderController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import shop.S5G.shop.dto.PageResponseDto;
+import shop.S5G.shop.dto.order.OrderDetailWithBookResponseDto;
 import shop.S5G.shop.dto.order.OrderWithDetailResponseDto;
 import shop.S5G.shop.service.order.OrderService;
 
@@ -17,7 +19,13 @@ public class OrderController {
     private final OrderService orderService;
 
     @GetMapping
-    public List<OrderWithDetailResponseDto> queryAllOrders(@RequestParam long customerId, Pageable pageable) {
-        return orderService.queryAllOrdersByCustomerId(customerId, pageable).getContent();
+    public PageResponseDto<OrderWithDetailResponseDto> queryAllOrders(@RequestParam long customerId, Pageable pageable) {
+        return PageResponseDto.of(orderService.queryAllOrdersByCustomerId(customerId, pageable));
+    }
+
+    @GetMapping("/details")
+    public List<OrderDetailWithBookResponseDto> getOrderDetails(@RequestParam long orderId) {
+        // TODO: Not Implemented yet.
+        return List.of();
     }
 }
--- src/test/java/shop/S5G/shop/controller/order/OrderControllerTest.java
+++ src/test/java/shop/S5G/shop/controller/order/OrderControllerTest.java
@@ -1,22 +1,28 @@
 package shop.S5G.shop.controller.order;
 
-import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.containsString;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import shop.S5G.shop.dto.order.OrderWithDetailResponseDto;
 import shop.S5G.shop.service.order.OrderService;
 
 @WebMvcTest(OrderController.class)
@@ -34,7 +40,28 @@ class OrderControllerTest {
             .param("customerId", "3")
         )
             .andExpect(status().isOk())
-            .andExpect(content().string(equalTo("[]")));
+            .andExpect(content().string(containsString("[]")))
+            .andDo(print());
+        verify(orderService, times(1)).queryAllOrdersByCustomerId(eq(3L), any());
+    }
+
+    @Test
+    void fetchOrdersTest() throws Exception {
+        OrderWithDetailResponseDto dto = new OrderWithDetailResponseDto(
+            1L, LocalDateTime.now(), 3000L, 5000L, "test title", 3, 4
+        );
+        List<OrderWithDetailResponseDto> result =  List.of(dto, dto);
+
+        when(orderService.queryAllOrdersByCustomerId(anyLong(), any())).thenReturn(
+            new PageImpl<>(result, Pageable.unpaged(), result.size())
+        );
+        mvc.perform(MockMvcRequestBuilders.get("/api/shop/orders")
+                .param("customerId", "3")
+            )
+            .andExpect(status().isOk())
+            .andExpect(content().string(containsString("\"representTitle\":\"test title\"")))
+            .andDo(print());
+
         verify(orderService, times(1)).queryAllOrdersByCustomerId(eq(3L), any());
     }
 }
