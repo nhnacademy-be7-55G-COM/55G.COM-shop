5b41231b086b2522b1d87981a6c6b62e7dccfbb3
FlowerfulFort <skagyghk@naver.com>
2024-10-31 10:02:37 +0900

    Fix: feign client headermap was fixed to requestheader

--- src/main/java/shop/S5G/shop/adapter/TossPaymentsAdapter.java
+++ src/main/java/shop/S5G/shop/adapter/TossPaymentsAdapter.java
@@ -1,6 +1,5 @@
 package shop.S5G.shop.adapter;
 
-import feign.HeaderMap;
 import java.util.Map;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
@@ -8,22 +7,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import shop.S5G.shop.dto.payments.TossPaymentsConfirmRequestDto;
+import org.springframework.web.bind.annotation.RequestHeader;
 import shop.S5G.shop.dto.payments.TossPaymentsCancelRequestDto;
+import shop.S5G.shop.dto.payments.TossPaymentsConfirmRequestDto;
 import shop.S5G.shop.dto.payments.TossPaymentsDto;
 
 @FeignClient(name = "tossPayments", url = "https://api.tosspayments.com/v1/payments")
 public interface TossPaymentsAdapter {
     @PostMapping("/confirm")
-    ResponseEntity<TossPaymentsDto> confirm(@HeaderMap Map<String, Object> headers, @RequestBody TossPaymentsConfirmRequestDto confirmRequest);
+    ResponseEntity<TossPaymentsDto> confirm(@RequestHeader Map<String, Object> headers, @RequestBody TossPaymentsConfirmRequestDto confirmRequest);
 
     @GetMapping("/{paymentKey}")
-    ResponseEntity<TossPaymentsDto> queryPayByPaymentKey(@HeaderMap Map<String, Object> headers, @PathVariable String paymentKey);
+    ResponseEntity<TossPaymentsDto> queryPayByPaymentKey(@RequestHeader Map<String, Object> headers, @PathVariable String paymentKey);
 
     @GetMapping("/orders/{orderId}")
-    ResponseEntity<TossPaymentsDto> queryPayByOrderId(@HeaderMap Map<String, Object> headers, @PathVariable String orderId);
+    ResponseEntity<TossPaymentsDto> queryPayByOrderId(@RequestHeader Map<String, Object> headers, @PathVariable String orderId);
 
     @PostMapping("/{paymentKey}/cancel")
-    ResponseEntity<TossPaymentsDto> cancel(@HeaderMap Map<String, Object> headers, @PathVariable String paymentKey, @RequestBody
+    ResponseEntity<TossPaymentsDto> cancel(@RequestHeader Map<String, Object> headers, @PathVariable String paymentKey, @RequestBody
         TossPaymentsCancelRequestDto cancelRequest);
 }
