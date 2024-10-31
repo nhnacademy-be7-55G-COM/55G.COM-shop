496e17552894448bbd89224545cb12db319f5a46
FlowerfulFort <skagyghk@naver.com>
2024-10-31 19:31:17 +0900

    Feat: 토스페이먼츠 결제 성공

--- src/main/java/shop/S5G/shop/adapter/TossPaymentsAdapter.java
+++ src/main/java/shop/S5G/shop/adapter/TossPaymentsAdapter.java
@@ -1,29 +1,28 @@
 package shop.S5G.shop.adapter;
 
-import java.util.Map;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
+import shop.S5G.shop.config.FeignAuthorizationConfig;
 import shop.S5G.shop.dto.payments.TossPaymentsCancelRequestDto;
 import shop.S5G.shop.dto.payments.TossPaymentsConfirmRequestDto;
 import shop.S5G.shop.dto.payments.TossPaymentsDto;
 
-@FeignClient(name = "tossPayments", url = "https://api.tosspayments.com/v1/payments")
+@FeignClient(name = "tossPayments", url = "https://api.tosspayments.com/v1/payments", configuration = FeignAuthorizationConfig.class)
 public interface TossPaymentsAdapter {
     @PostMapping("/confirm")
-    ResponseEntity<TossPaymentsDto> confirm(@RequestHeader Map<String, Object> headers, @RequestBody TossPaymentsConfirmRequestDto confirmRequest);
+    ResponseEntity<TossPaymentsDto> confirm(@RequestBody TossPaymentsConfirmRequestDto confirmRequest);
 
     @GetMapping("/{paymentKey}")
-    ResponseEntity<TossPaymentsDto> queryPayByPaymentKey(@RequestHeader Map<String, Object> headers, @PathVariable String paymentKey);
+    ResponseEntity<TossPaymentsDto> queryPayByPaymentKey(@PathVariable String paymentKey);
 
     @GetMapping("/orders/{orderId}")
-    ResponseEntity<TossPaymentsDto> queryPayByOrderId(@RequestHeader Map<String, Object> headers, @PathVariable String orderId);
+    ResponseEntity<TossPaymentsDto> queryPayByOrderId(@PathVariable String orderId);
 
     @PostMapping("/{paymentKey}/cancel")
-    ResponseEntity<TossPaymentsDto> cancel(@RequestHeader Map<String, Object> headers, @PathVariable String paymentKey, @RequestBody
+    ResponseEntity<TossPaymentsDto> cancel(@PathVariable String paymentKey, @RequestBody
         TossPaymentsCancelRequestDto cancelRequest);
 }
--- src/main/java/shop/S5G/shop/service/payments/TossPaymentsAdapterCaller.java
+++ src/main/java/shop/S5G/shop/service/payments/TossPaymentsAdapterCaller.java
@@ -4,9 +4,9 @@ import shop.S5G.shop.dto.payments.TossPaymentsCancelRequestDto;
 import shop.S5G.shop.dto.payments.TossPaymentsConfirmRequestDto;
 import shop.S5G.shop.dto.payments.TossPaymentsDto;
 
-public interface TossPaymentsService {
+public interface TossPaymentsAdapterCaller {
 
-    TossPaymentsDto confirmPayment(TossPaymentsConfirmRequestDto confirmRequest);
+    TossPaymentsDto confirmPayment(long orderDataId, TossPaymentsConfirmRequestDto confirmRequest);
 
     TossPaymentsDto cancelPayment(String paymentKey, TossPaymentsCancelRequestDto cancelRequest);
 }
