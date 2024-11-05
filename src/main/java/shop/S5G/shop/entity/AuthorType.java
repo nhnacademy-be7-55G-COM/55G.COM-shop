2eb0776b8adef7860abc3c7cee60017b50be1ad8
Gyubin-Han <yangglei2@naver.com>
2024-11-05 14:40:17 +0900

    Feat: AuthorType CRUD 구현

--- src/main/java/shop/S5G/shop/entity/AuthorType.java
+++ src/main/java/shop/S5G/shop/entity/AuthorType.java
@@ -1,4 +1,17 @@
 package shop.S5G.shop.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
 public class AuthorType {
+
+    @Id
+    private long authorTypeId;
+    private String typeName;
+    private boolean active;
 }
---
+++ src/main/java/shop/S5G/shop/repository/AuthorTypeRepository.java
@@ -0,0 +1,8 @@
+package shop.S5G.shop.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import shop.S5G.shop.entity.AuthorType;
+
+public interface AuthorTypeRepository extends JpaRepository<AuthorType, Long> {
+
+}
