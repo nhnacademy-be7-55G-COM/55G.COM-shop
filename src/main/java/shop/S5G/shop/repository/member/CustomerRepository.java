b010f8a722c59b605728020722e5a60e3d3d7290
FlowerfulFort <skagyghk@naver.com>
2024-11-08 01:24:52 +0900

    Feat: Member repo & service update
    footer: repository & service tests

--- src/main/java/shop/S5G/shop/repository/member/CustomerRepository.java
+++ src/main/java/shop/S5G/shop/repository/member/CustomerRepository.java
@@ -1,5 +1,6 @@
 package shop.S5G.shop.repository.member;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import shop.S5G.shop.entity.member.Customer;
 import shop.S5G.shop.repository.member.qdsl.CustomerQuerydslRepository;
@@ -7,4 +8,5 @@ import shop.S5G.shop.repository.member.qdsl.CustomerQuerydslRepository;
 public interface CustomerRepository extends JpaRepository<Customer, Long>,
     CustomerQuerydslRepository {
 
+    Optional<Customer> findByCustomerIdAndActiveIsTrue(long id);
 }
