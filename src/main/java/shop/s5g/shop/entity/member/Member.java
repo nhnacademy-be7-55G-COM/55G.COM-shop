1bb08bd274b0297449d1ca55c825081e2c62a1a8
FlowerfulFort <skagyghk@naver.com>
2024-11-08 20:09:43 +0900

    Feat: 회원가입 포인트 증정

--- src/main/java/shop/s5g/shop/entity/member/Member.java
+++ src/main/java/shop/s5g/shop/entity/member/Member.java
@@ -12,6 +12,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Getter
@@ -41,6 +42,7 @@ public class Member {
     private String birth;
     private LocalDateTime createdAt;
     private LocalDateTime latestLoginAt;
+    @Setter
     private Long point;
 
 }
