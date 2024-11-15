ddecad2c9ad5556a1d35e7ceef7948a201363453
FlowerfulFort <skagyghk@naver.com>
2024-11-15 14:48:07 +0900

    Test/Fix: Active만 서치하도록 개선.

--- src/main/java/shop/s5g/shop/repository/BookStatusRepository.java
+++
@@ -1,8 +0,0 @@
-//package shop.s5g.shop.repository;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import shop.s5g.shop.entity.BookStatus;
-//
-//public interface BookStatusRepository extends JpaRepository<BookStatus,Long> {
-//
-//}
