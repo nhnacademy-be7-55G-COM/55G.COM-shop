f9a878c11d97c4645e61f8e9f6523325f09fc93d
FlowerfulFort <skagyghk@naver.com>
2024-11-28 16:09:42 +0900

    Fix: 주문 조회 핫픽스

--- src/main/java/shop/s5g/shop/dto/order/OrderCreateResponseDto.java
+++ src/main/java/shop/s5g/shop/dto/order/OrderCreateResponseDto.java
@@ -2,9 +2,9 @@ package shop.s5g.shop.dto.order;
 
 import shop.s5g.shop.entity.order.Order;
 
-public record OrderCreateResponseDto(long orderId) {
+public record OrderCreateResponseDto(long orderId, String uuid) {
 
     public static OrderCreateResponseDto of(Order order) {
-        return new OrderCreateResponseDto(order.getId());
+        return new OrderCreateResponseDto(order.getId(), order.getUuid());
     }
 }
