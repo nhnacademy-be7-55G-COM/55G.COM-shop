0745972e41baa01fb69941ca585bbf6b33e0d957
bo <cbs1006@naver.com>
2024-11-29 16:33:19 +0900

    Feat: 휴면 회원 구현

--- src/main/java/shop/s5g/shop/controller/member/MemberController.java
+++ src/main/java/shop/s5g/shop/controller/member/MemberController.java
@@ -2,7 +2,6 @@ package shop.s5g.shop.controller.member;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -17,7 +16,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import shop.s5g.shop.config.RedisConfig;
 import shop.s5g.shop.dto.customer.CustomerUpdateRequestDto;
 import shop.s5g.shop.dto.member.IdCheckResponseDto;
 import shop.s5g.shop.dto.member.LoginResponseDto;
