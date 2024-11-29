91b3cf4c72eae4e252dcc0f4d84a93faf2faa120
kimminjun <jj36282013@gmail.com>
2024-11-29 10:46:04 +0900

    Feat: refresh token 만료로 인한 로그아웃시 redis 에서 db로 저장

--- src/main/java/shop/s5g/shop/listener/RedisKeyExpirationListener.java
+++ src/main/java/shop/s5g/shop/listener/RedisKeyExpirationListener.java
@@ -1,5 +1,6 @@
 package shop.s5g.shop.listener;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.data.redis.connection.Message;
@@ -9,7 +10,7 @@ import org.springframework.stereotype.Component;
 import shop.s5g.shop.config.RedisConfig;
 import shop.s5g.shop.service.cart.CartService;
 
-
+@Slf4j
 @ConditionalOnBean(RedisConfig.class)
 @Component
 public class RedisKeyExpirationListener extends KeyExpirationEventMessageListener {
@@ -27,5 +28,14 @@ public class RedisKeyExpirationListener extends KeyExpirationEventMessageListene
     @Override
     public void onMessage(Message message, byte[] pattern) {
 
+        if (message.toString().startsWith("refresh_token")) {
+
+            String customerLoginId = message.toString().split(":")[1];
+            try {
+                cartService.FromRedisToDb(customerLoginId);
+            } catch (Exception e) {
+                log.debug("refresh_token 만료시 장바구니 db 저장에 실패했습니다.");
+            }
+        }
     }
 }
